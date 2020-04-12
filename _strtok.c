af9922591c8662a2a3e3f666b418de217b69fafe
hamdi458 <hamdi.ghorbel645@gmail.com>
2020-04-12 19:21:19 +0000

    aa

--- _strtok.c
+++ _strtok.c
@@ -1,48 +1,57 @@
-#include <stdio.h>
-#include<string.h>
 #include "shell.h"
-
+/**
+ * _strtok - strtok.
+ *@str : string
+ *@del : delimiters
+ * Return: 0.
+ */
 char *ch = NULL;
 char *_strtok(char *str, char *del)
 {
-	int i;
-	int len = strlen(del);
+	int i, len = _strlen(del);
 	char *start_copy;
-	if (ch == NULL && str)
-	{
-		ch = str;
-	}
-	if ((!str && ch == NULL) || !del)
-		return(NULL);
 
+	if (ch == NULL && str)
+		ch = str;
+	if ((!str && ch == NULL) || !del)
+		return (NULL);
 	start_copy = ch;
-	while(1) {
-		for(i = 0; i < len; i ++) {
-			if(*start_copy == del[i]) {
-				start_copy ++;
+	while (1)
+	{
+		i = 0;
+		while (del[i])
+		{
+			if (*start_copy == del[i])
+			{
+				start_copy++;
 				break;
 			}
+			i++;
 		}
-		if(i == len) {
+		if (i == len)
 			break;
-		}
 	}
 	ch = start_copy;
-	if(*start_copy == '\0') {
+	if (*start_copy == '\0')
+	{
 		ch = NULL;
 		return NULL;
 	}
-
-	while(*ch != '\0') {
-		for(i = 0; i < len; i ++) {
-			if(*ch == del[i]) {
+	while (*ch != '\0')
+	{
+		i = 0;
+		while (del[i])
+		{
+			if (*ch == del[i])
+			{
 				*ch = '\0';
 				break;
 			}
+			i++;
 		}
 		ch ++;
 		if (i < len)
 			break;
 	}
-	return start_copy;
+	return (start_copy);
 }
